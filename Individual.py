04d7578c616bd8457d72a882f33ae7a919d16824
RoyCaspi <caspi7@hotmail.com>
2022-01-05 21:01:41 +0200

    wrote the first crossover func 'mate'

--- Individual.py
+++ Individual.py
@@ -35,6 +35,45 @@ class Individual:
             self.__gene[i], self.__gene[j] = self.__gene[j], self.__gene[i]
 
 
-    # def mate(self, partner):
+    def mate(self, partner):
+        i, j = np.random.randint(0, self.__n, 2) #step 1:generates the range for crossover
+        if i > j:
+            i, j = j, i
+        child1 = np.ones(len(self.__gene)) * -1
+        child2 = np.ones(len(self.__gene)) * -1
+        child1[i:j] = self.__gene[i:j] #step 2: copies parents' segments into children
+        child2[i:j] = partner.__gene[i:j] #todo: check if reachable or need to remove '__'
+        numbers_done = set(np.concatenate((child1, child2)))
+        for i in range(1, len(self.__gene) + 1): #step 3: copies the numbers not in 'numbers_done' to children
+            if i not in numbers_done:
+                index1 = list(partner.__gene).index(i)
+                child1[index1] = i
+                index2 = list(self.__gene).index(i)
+                child1[index2] = i
+                numbers_done.add(i) #maybe a waste of memory
+        for i in range(child1): #step4: fills in the rest of childrens' gene
+            if child1[i] == -1:
+                num_to_fill = partner.__gene[i]
+                if num_to_fill not in child1:
+                    child1[i] = num_to_fill
+                else:
+                    while (True):
+                        num_to_fill = child2[list(child1).index(num_to_fill)]
+                        if num_to_fill not in child1:
+                            child1[i] = num_to_fill
+                            break;
+            if child2[i] == -1:
+                num_to_fill = self.__gene[i]
+                if num_to_fill not in child2:
+                    child2[i] = num_to_fill
+                else:
+                    while (True):
+                        num_to_fill = child1[list(child2).index(num_to_fill)]
+                        if num_to_fill not in child2:
+                            child2[i] = num_to_fill
+                            break;
+        return child1, child2
+
+
 
 
